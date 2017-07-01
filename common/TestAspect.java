3b857d20c0e7e6477bfdd975d945a037677cbfa2
yaoguangluo <lyg.tin@gmail.com>
2017-06-30 18:34:22 -0700

    Add files via upload

---
+++ common/TestAspect.java
@@ -0,0 +1,39 @@
+package common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TestAspect {
+
+    public void pointCut() {
+    }
+
+    @AfterReturning("execution(* controller.*.*(..))")
+    public void doAfter(JoinPoint jp) {
+        System.out.println("log Ending method: " + jp.getTarget().getClass().getName() + "." + jp.getSignature().getName());
+    }
+
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+        long time = System.currentTimeMillis();
+        Object retVal = pjp.proceed();
+        time = System.currentTimeMillis() - time;
+        System.out.println("process time: " + time + " ms");
+        return retVal;
+    }
+
+    @Before("execution(* controller.*.*(..))")
+    public void doBefore(JoinPoint jp) {
+        System.out.println("log Begining method: " + jp.getTarget().getClass().getName() + "." + jp.getSignature().getName());
+    }
+
+    public void doThrowing(JoinPoint jp, Throwable ex) {
+        System.out.println("method " + jp.getTarget().getClass().getName() + "." + jp.getSignature().getName() + " throw exception");
+        System.out.println(ex.getMessage());
+    }
+}
